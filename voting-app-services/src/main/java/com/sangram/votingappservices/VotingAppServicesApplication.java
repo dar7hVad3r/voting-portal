ed6db0c5b43f837ae75bb797271724f245d290db
Sangram Barge <sangrambarge098@gmail.com>
2022-10-28 21:14:13 +0530

    project init

---
+++ voting-app-services/src/main/java/com/sangram/votingappservices/VotingAppServicesApplication.java
@@ -0,0 +1,13 @@
+package com.sangram.votingappservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VotingAppServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VotingAppServicesApplication.class, args);
+	}
+
+}
---
+++ voting-app-services/src/test/java/com/sangram/votingappservices/VotingAppServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sangram.votingappservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VotingAppServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
